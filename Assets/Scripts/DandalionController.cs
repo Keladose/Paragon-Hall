532bb10994b8b708b45a2b3d705aba959693dd4c
Yusuf <169350709+Keladose@users.noreply.github.com>
2025-04-22 01:17:45 +1000

    dantalion

---
+++ Assets/Scripts/DandalionController.cs
@@ -0,0 +1,115 @@
+using System.Collections;
+using System.Collections.Generic;
+using Spellect;
+using Unity.Mathematics;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class DandalionController : BaseEnemy
+{
+
+    public float stopDistance;
+    public List<SpellData> spellData;
+
+    private bool inRange;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+
+    protected override void Update()
+    {
+        if (player == null) return; 
+        float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+        isChasing = distanceToPlayer <= chaseRange;
+
+        if (isChasing)
+        {
+            ChasePlayer();
+        }
+        else
+        {
+            Patrol();
+        }
+
+        if (inRange)
+        {
+            ShootProjectile();
+            if (Random.Range(0, 500) == 2)
+            {
+                ActivateSpecialAbility();
+            }
+        }
+
+        
+    }
+
+    protected override void AttackPlayer()
+    {
+
+    }
+    protected override void ChasePlayer()
+    {
+        float distance = Vector2.Distance(transform.position, player.position);
+
+        
+        if (distance > stopDistance)
+        {
+            UpdateSpriteDirection(player.position);
+            AddForceTowardsTarget(player.position, moveSpeed);
+            inRange = false;
+        }
+        else
+        {
+           
+            rb.velocity = Vector2.zero;
+            inRange = true;
+        }
+    }
+
+    protected void ShootProjectile()
+    {
+        if (spellData.Count > 0)
+        {
+            for (int i = 0; i < spellData.Count; i++)
+            {
+                if (Random.Range(0, 400) == 1)
+                {
+                    GameObject projectile = Instantiate(spellData[i].projectilePrefab, transform.position,
+                        Quaternion.identity);
+                    Rigidbody2D rb = projectile.GetComponent<Rigidbody2D>();
+                    Vector2 direction = (player.position - transform.position).normalized;
+                    float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+                    projectile.transform.rotation = Quaternion.Euler(0, 0, angle);
+                    rb.velocity = direction * spellData[0].speed;
+                }
+            }
+        }
+    }
+
+    protected void ActivateSpecialAbility()
+    {
+        int projectileCount = 12;
+        
+        if (spellData.Count > 0)
+        {
+            int randomIndex = Random.Range(0, spellData.Count);
+
+            for (int i = 0; i < projectileCount; i++)
+            {
+                float angle = i * (360f / projectileCount);
+                float rad = angle * Mathf.Deg2Rad; 
+                Vector2 direction = new Vector2(Mathf.Cos(rad), Mathf.Sin(rad)).normalized;
+                Vector2 spawnPos = (Vector2)transform.position + direction;
+                
+                GameObject projectile = Instantiate(spellData[randomIndex].projectilePrefab, spawnPos, Quaternion.identity);
+                Rigidbody2D rb = projectile.GetComponent<Rigidbody2D>();
+
+                projectile.transform.rotation = Quaternion.Euler(0, 0, angle);
+                rb.velocity = direction * spellData[randomIndex].speed;
+            }
+        }
+    }
+}
